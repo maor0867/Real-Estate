7cbf018ddb782ba84e7034a2697b808e06791769
maor0867 <maorhanan@gmail.com>
2023-10-12 15:47:16 +0300

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Profile from './pages/Profile'
 import SighIn from './pages/SighIn'
 import SighUp from './pages/SighUp'
 import About from './pages/About'
+import Header from "./components/Header"
 function App() {
 
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/profile" element={<Profile/>}/>
